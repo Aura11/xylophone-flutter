74cc3975736e18ff915a9f4ea23a8aa25a1e5565
Aura11 <81680756+Aura11@users.noreply.github.com>
2023-02-14 01:43:05 +0300

    neww

--- lib/main.dart
+++ lib/main.dart
@@ -9,68 +9,84 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
           body: SafeArea(
-              child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          TextButton(
-            style: TextButton.styleFrom(backgroundColor: Colors.red),
-            onPressed: () {
-              AssetsAudioPlayer.newPlayer().open(
-                Audio("assets/note1.wav"),
-              );
-            },
-            child: null,
-          ),
-          TextButton(
-              style: TextButton.styleFrom(backgroundColor: Colors.orange),
-              onPressed: () {
-                AssetsAudioPlayer.newPlayer().open(
-                  Audio("assets/note2.wav"),
-                );
-              },
-              child: null),
-          TextButton(
-              style: TextButton.styleFrom(backgroundColor: Colors.yellow),
-              onPressed: () {
-                AssetsAudioPlayer.newPlayer().open(
-                  Audio("assets/note3.wav"),
-                );
-              },
-              child: null),
-          TextButton(
-              style: TextButton.styleFrom(backgroundColor: Colors.lightGreen),
-              onPressed: () {
-                AssetsAudioPlayer.newPlayer().open(
-                  Audio("assets/note4.wav"),
-                );
-              },
-              child: null),
-          TextButton(
-              style: TextButton.styleFrom(backgroundColor: Colors.green),
-              onPressed: () {
-                AssetsAudioPlayer.newPlayer().open(
-                  Audio("assets/note5.wav"),
-                );
-              },
-              child: null),
-          TextButton(
-              style: TextButton.styleFrom(backgroundColor: Colors.blue),
-              onPressed: () {
-                AssetsAudioPlayer.newPlayer().open(
-                  Audio("assets/note6.wav"),
-                );
-              },
-              child: null),
-          TextButton(
-              style: TextButton.styleFrom(backgroundColor: Colors.purple),
-              onPressed: () {
-                AssetsAudioPlayer.newPlayer().open(
-                  Audio("assets/note7.wav"),
-                );
-              },
-              child: null),
-        ],
-      ))),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+              child: TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.red),
+                onPressed: () {
+                  AssetsAudioPlayer.newPlayer().open(
+                    Audio("assets/note1.wav"),
+                  );
+                },
+                child: null,
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.orange),
+                  onPressed: () {
+                    AssetsAudioPlayer.newPlayer().open(
+                      Audio("assets/note2.wav"),
+                    );
+                  },
+                  child: null),
+            ),
+            Expanded(
+              child: TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.yellow),
+                  onPressed: () {
+                    AssetsAudioPlayer.newPlayer().open(
+                      Audio("assets/note3.wav"),
+                    );
+                  },
+                  child: null),
+            ),
+            Expanded(
+              child: TextButton(
+                  style:
+                      TextButton.styleFrom(backgroundColor: Colors.lightGreen),
+                  onPressed: () {
+                    AssetsAudioPlayer.newPlayer().open(
+                      Audio("assets/note4.wav"),
+                    );
+                  },
+                  child: null),
+            ),
+            Expanded(
+              child: TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.green),
+                  onPressed: () {
+                    AssetsAudioPlayer.newPlayer().open(
+                      Audio("assets/note5.wav"),
+                    );
+                  },
+                  child: null),
+            ),
+            Expanded(
+              child: TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.blue),
+                  onPressed: () {
+                    AssetsAudioPlayer.newPlayer().open(
+                      Audio("assets/note6.wav"),
+                    );
+                  },
+                  child: null),
+            ),
+            Expanded(
+              child: TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.purple),
+                  onPressed: () {
+                    AssetsAudioPlayer.newPlayer().open(
+                      Audio("assets/note7.wav"),
+                    );
+                  },
+                  child: null),
+            ),
+          ],
+        ),
+      )),
     );
   }
 }
